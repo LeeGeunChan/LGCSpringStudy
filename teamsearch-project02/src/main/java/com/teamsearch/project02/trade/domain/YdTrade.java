60794195d136d721369a364a464d8145d4ef9558
CLASS502LGC <CLASS502LGC@PC502LGC>
2018-02-23 12:06:43 +0900

    domain

--- teamsearch-project02/src/main/java/com/teamsearch/project02/trade/domain/YdTrade.java
+++ teamsearch-project02/src/main/java/com/teamsearch/project02/trade/domain/YdTrade.java
@@ -1,5 +1,72 @@
 package com.teamsearch.project02.trade.domain;
 
 public class YdTrade {
+	
+	private int ydTradeId;
+	private String address1;
+	private String address2;
+	private float area;
+	private String contractDate;
+	private int price;
+	private int floor;
+	private int bYear;
+	private String contractMonth;
+	
+	public int getYdTradeId() {
+		return ydTradeId;
+	}
+	public void setYdTradeId(int ydTradeId) {
+		this.ydTradeId = ydTradeId;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public float getArea() {
+		return area;
+	}
+	public void setArea(float area) {
+		this.area = area;
+	}
+	public String getContractDate() {
+		return contractDate;
+	}
+	public void setContractDate(String contractDate) {
+		this.contractDate = contractDate;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getFloor() {
+		return floor;
+	}
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+	public int getbYear() {
+		return bYear;
+	}
+	public void setbYear(int bYear) {
+		this.bYear = bYear;
+	}
+	public String getContractMonth() {
+		return contractMonth;
+	}
+	public void setContractMonth(String contractMonth) {
+		this.contractMonth = contractMonth;
+	}
+	
+	
 
 }
