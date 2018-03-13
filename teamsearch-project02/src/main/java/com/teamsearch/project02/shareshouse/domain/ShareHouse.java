f9bdb61fc0408c703c0aaf7d70cdb23d85bae77f
CLASS502LGC <CLASS502LGC@PC502LGC>
2018-03-13 12:21:54 +0900

    sharehouse 작업중

---
+++ teamsearch-project02/src/main/java/com/teamsearch/project02/shareshouse/domain/ShareHouse.java
@@ -0,0 +1,16 @@
+package com.teamsearch.project02.shareshouse.domain;
+
+public class ShareHouse {
+	
+	private int shNo;
+	private String shName;
+	private String shZipCode;
+	private String shAddress1;
+	private String shAddress2;
+	private String shGender;
+	private String shType;
+	private String shContent;
+	private String shImage;
+	private String shPlan;
+	
+}
